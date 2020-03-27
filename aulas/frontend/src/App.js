bd991d06643de18ae167950800ee3ccd1b82e374
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2020-03-27 14:06:37 -0300

    Frontend do aplicativo pronto

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Routes from './routes';
+
+import Logon from './pages/Logon';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  // Array [valor, funcaoDeAtualizacao]
-
-  function increment(){
-    setCounter(counter+ 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Routes/>
   );
 }
 
