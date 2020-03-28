e8ff764809d3b4eeb1f75c6ba971a4d5983742fc
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2020-03-28 13:58:36 -0300

    Validações prontas. Projeto Omnistack pronto

--- aulas/backend/src/app.js
+++ aulas/backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); /* Importando as rotas  */
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
 
 /*
 
@@ -35,4 +39,3 @@ Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos
   Query Builder: table('users').select('*').where()
 */
 
-app.listen(3333);
